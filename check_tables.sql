5bd48f02f65a5014676501a605050ebd75ec7dd7
John Hendricks <105813545+j-hendricks@users.noreply.github.com>
2022-08-08 15:42:46 -0400

    Delete check_tables.sql

--- check_tables.sql
+++
@@ -1,8 +0,0 @@
--- read both tables
-SELECT * FROM movies;
-SELECT * FROM ratings;
-
---count number of rows by calling count function on any column
-SELECT COUNT(imdb_id) as "Total Rows in Movie Table" FROM movies;
-
-SELECT COUNT(rating) as "Total Rows in Rating Table" FROM ratings;
